222a10b51f64d2d6bac720006ec5f6afbb7f9e3f
Luke <lnalewaj@andrew.cmu.edu>
2024-09-20 23:25:13 -0400

    luke changes from project 1

--- public/src/admin/admin.js
+++ public/src/admin/admin.js
@@ -179,25 +179,29 @@ app.onDomReady();
 
 	function setupRestartLinks() {
 		require(['benchpress', 'bootbox', 'admin/modules/instance'], function (benchpress, bootbox, instance) {
-			// need to preload the compiled alert template
-			// otherwise it can be unloaded when rebuild & restart is run
-			// the client can't fetch the template file, resulting in an error
 			benchpress.render('partials/toast', {}).then(function () {
-				$('[component="rebuild-and-restart"]').off('click').on('click', function () {
-					bootbox.confirm('[[admin/admin:alert.confirm-rebuild-and-restart]]', function (confirm) {
-						if (confirm) {
-							instance.rebuildAndRestart();
-						}
-					});
-				});
+				setupRebuildAndRestartLink(instance, bootbox);
+				setupRestartLink(instance, bootbox);
+			});
+		});
+	}
 
-				$('[component="restart"]').off('click').on('click', function () {
-					bootbox.confirm('[[admin/admin:alert.confirm-restart]]', function (confirm) {
-						if (confirm) {
-							instance.restart();
-						}
-					});
-				});
+	function setupRebuildAndRestartLink(instance, bootbox) {
+		$('[component="rebuild-and-restart"]').off('click').on('click', function () {
+			bootbox.confirm('[[admin/admin:alert.confirm-rebuild-and-restart]]', function (confirm) {
+				if (confirm) {
+					instance.rebuildAndRestart();
+				}
+			});
+		});
+	}
+
+	function setupRestartLink(instance, bootbox) {
+		$('[component="restart"]').off('click').on('click', function () {
+			bootbox.confirm('[[admin/admin:alert.confirm-restart]]', function (confirm) {
+				if (confirm) {
+					instance.restart();
+				}
 			});
 		});
 	}
