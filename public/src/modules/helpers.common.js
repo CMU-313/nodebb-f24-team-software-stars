70e9a662c497218215d9f6583636d4c19c203f77
Emma Tong <53305901+etong11@users.noreply.github.com>
2024-10-09 14:01:38 -0400

    Added endorsed to topic fields/class

--- public/src/modules/helpers.common.js
+++ public/src/modules/helpers.common.js
@@ -156,7 +156,7 @@ module.exports = function (utils, Benchpress, relative_path) {
 	}
 
 	function generateTopicClass(topic) {
-		const fields = ['locked', 'pinned', 'deleted', 'unread', 'scheduled'];
+		const fields = ['locked', 'pinned', 'deleted', 'unread', 'scheduled', 'endorsed'];
 		return fields.filter(field => !!topic[field]).join(' ');
 	}
 
